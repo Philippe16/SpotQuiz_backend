92caa73d11c0b40af284cb2448f5fe66217339ce
Angeliza Medina <78863493+Angeliza-Medina@users.noreply.github.com>
2022-05-18 10:28:56 +0200

    Fix test

--- src/test/java/facades/QuizFacadeTest.java
+++ src/test/java/facades/QuizFacadeTest.java
@@ -3,7 +3,6 @@ package facades;
 import dtos.MusicDTO;
 import dtos.NewQuestionDTO;
 import dtos.NewQuizDTO;
-import dtos.QuizDTO;
 import entities.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,52 +22,91 @@ class QuizFacadeTest {
     private static EntityManagerFactory emf = EMF_Creator.createEntityManagerFactoryForTest();
     private static QuizFacade quizFacade = QuizFacade.getQuizFacade(emf);
 
+    Role userRole;
+    User user;
+
+    private void resetDB(EntityManager em){
+        em.getTransaction().begin();
+            user = em.find(User.class, 1);
+
+            user.setCreatedQuizzes(new ArrayList<>());
+            em.merge(user);
+
+            em.createQuery("DELETE FROM Question").executeUpdate();
+            em.createQuery("DELETE FROM Music").executeUpdate();
+            em.createQuery("DELETE FROM Quiz").executeUpdate();
+            em.createQuery("DELETE FROM User").executeUpdate();
+            em.createQuery("DELETE FROM Role").executeUpdate();
+
+        em.createNativeQuery("ALTER TABLE roles AUTO_INCREMENT = 1").executeUpdate();
+        em.createNativeQuery("ALTER TABLE users AUTO_INCREMENT = 1").executeUpdate();
+        em.createNativeQuery("ALTER TABLE quizzes AUTO_INCREMENT = 1").executeUpdate();
+        em.createNativeQuery("ALTER TABLE questions AUTO_INCREMENT = 1").executeUpdate();
+
+        em.getTransaction().commit();
+    }
+
     @BeforeEach
     void setUp() {
+        EntityManager em = emf.createEntityManager();
 
+        try{
+            userRole = new Role("user");
+            user = new User("User123", "user@gmail.com", "123", userRole);
+
+            em.getTransaction().begin();
+            em.persist(userRole);
+            em.persist(user);
+            em.getTransaction().commit();
+        }finally{
+            em.close();
+        }
     }
 
     @AfterEach
     void tearDown() {
+        EntityManager em = emf.createEntityManager();
+
+        try{
+            resetDB(em);
+        }finally{
+            em.close();
+        }
     }
 
     @Test
     public void createQuizTest(){
         EntityManager em = emf.createEntityManager();
 
-        Role userRole = new Role("user");
-        User user = new User("User123", "user@gmail.com", "123", userRole);
+        try{
+            TypedQuery<User> query = em.createQuery("SELECT user FROM User AS user WHERE user.email = :email", User.class);
+            query.setParameter("email", "user@gmail.com");
+            User userFromDB = query.getSingleResult();
 
-        em.getTransaction().begin();
-            em.persist(userRole);
-            em.persist(user);
-        em.getTransaction().commit();
-
-        TypedQuery<User> query = em.createQuery("SELECT user FROM User AS user WHERE user.email = :email", User.class);
-        query.setParameter("email", "user@gmail.com");
-        User userFromDB = query.getSingleResult();
-
-        List<NewQuestionDTO> questionDTOList = new ArrayList<>();
-        NewQuizDTO quizDTO = new NewQuizDTO(userFromDB.getUserID(),"TestQuizTitle", questionDTOList);
-        Music music = new Music("1","TestTitle", "TestArtist", "TestCoverImg", "TestSnippet"); // Har skrevet lidt forneden
-        MusicDTO musicDTO = new MusicDTO(music);
-        NewQuestionDTO question1 = new NewQuestionDTO("TestQuestion", "Answer1", "Answer2", "Answer3", "CorrectAnswer", musicDTO);
-        NewQuestionDTO question2 = new NewQuestionDTO("TestQuestion", "Answer1", "Answer2", "Answer3", "CorrectAnswer", musicDTO);
-        NewQuestionDTO question3 = new NewQuestionDTO("TestQuestion", "Answer1", "Answer2", "Answer3", "CorrectAnswer", musicDTO);
+            List<NewQuestionDTO> questionDTOList = new ArrayList<>();
+            NewQuizDTO quizDTO = new NewQuizDTO(userFromDB.getUserID(),"TestQuizTitle", questionDTOList);
+            Music music = new Music("1","TestTitle", "TestArtist", "TestCoverImg", "TestSnippet"); // Har skrevet lidt forneden
+            MusicDTO musicDTO = new MusicDTO(music);
+            NewQuestionDTO question1 = new NewQuestionDTO("TestQuestion", "Answer1", "Answer2", "Answer3", "CorrectAnswer", musicDTO);
+            NewQuestionDTO question2 = new NewQuestionDTO("TestQuestion", "Answer1", "Answer2", "Answer3", "CorrectAnswer", musicDTO);
+            NewQuestionDTO question3 = new NewQuestionDTO("TestQuestion", "Answer1", "Answer2", "Answer3", "CorrectAnswer", musicDTO);
 
 
-        questionDTOList.add(question1);
-        questionDTOList.add(question2);
-        questionDTOList.add(question3);
+            questionDTOList.add(question1);
+            questionDTOList.add(question2);
+            questionDTOList.add(question3);
 
-        quizFacade.createQuiz(quizDTO);
+            quizFacade.createQuiz(quizDTO);
 
-        TypedQuery<Quiz> query2 = em.createQuery(
-            "SELECT quiz FROM Quiz AS quiz", Quiz.class
-        );
+            TypedQuery<Quiz> query2 = em.createQuery(
+                   "SELECT quiz FROM Quiz AS quiz", Quiz.class
+            );
 
-        List<Quiz> quizzes = query2.getResultList();
-        assertEquals(1, quizzes.size());
+            List<Quiz> quizzes = query2.getResultList();
+            assertEquals(1, quizzes.size());
+        }finally {
+            em.close();
+        }
     }
 
 }
